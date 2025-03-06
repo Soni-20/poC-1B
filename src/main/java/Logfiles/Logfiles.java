9e48ab970d2cf4ddfbbf8280e6de1379a3610275
soni <sonisebastian99@gmail.com>
2025-03-07 05:12:07 +0530

    few lines added

--- src/main/java/Logfiles/Logfiles.java
+++ src/main/java/Logfiles/Logfiles.java
@@ -1,8 +1,42 @@
 package Logfiles;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Logfiles {
     public static void main(String[] args)
     {
-        System.out.println("hii");
+        Scanner sc=new Scanner(System.in);
+        String inFolder;
+        while (true){
+        System.out.println("Enter Folder path:");
+        inFolder=sc.nextLine();
+        if (inFolder.isEmpty())
+        {
+            System.out.println("Error:Folder path cannot be empty.please enter a valid path.");
+            continue;
+        }
+        File folder=new File(inFolder);
+        if (!folder.exists()||folder.isDirectory()) {
+            System.out.println("Error:Folder does not exist or is not a directory.Try again");
+            continue;
+        }
+        break;}
+        sc.close();
+        final String folderPath=inFolder;
+
+        Thread processing=new Thread(()-> {
+            System.out.println("Processing folder:"+ folderPath);
+        try{
+            Thread.sleep(1000);
+        }
+        catch (InterruptedException e)
+        {
+            System.out.println("Thread was interrupted");
+        }
+        System.out.println("processing completed");
+        });
+        processing.start();
+
     }
 }
