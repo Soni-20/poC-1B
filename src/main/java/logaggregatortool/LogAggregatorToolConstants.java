41c0b3d039a770cd8744ab43eed437a31e6dee79
soni <sonisebastian99@gmail.com>
2025-03-11 18:19:49 +0530

    Code Moodified

--- src/main/java/logaggregatortool/LogAggregatorToolConstants.java
+++ src/main/java/logaggregatortool/LogAggregatorToolConstants.java
@@ -5,5 +5,6 @@ public class LogAggregatorToolConstants {
     public static final String INVALID_FILES = "Count of invalid files:";
     public static final String INVALID_FOLDER = "Invalid_Folderpath .please provide a valid folderpath.";
     public static final String PROCESSING_MESSAGE = "Processing";
+    public static final String TOTAL_FILES = "Total count of files:";
     public static final String VALID_FILES = "Count of valid Files:";
 }
--- src/main/java/logaggregatortool/LogAggregatorValidator.java
+++ src/main/java/logaggregatortool/LogAggregatorValidator.java
@@ -1,29 +1,33 @@
 package logaggregatortool;
 
 import java.io.File;
+
 public class LogAggregatorValidator {
-    public void validatorFolder(File User_input_path) {
-        String[] files = User_input_path.list();
-        if (!(User_input_path.exists())) {
+    public void validatorFolder(File Userinputpath) {
+        String[] files = Userinputpath.list();
+        if (!(Userinputpath.exists())) {
             System.out.println(LogAggregatorToolConstants.INVALID_FOLDER);
             return;
         }
-        if (User_input_path.length() == 0) {
+        if (Userinputpath.length() == 0) {
             System.out.println(LogAggregatorToolConstants.FOLDER_EMPTY);
         } else {
             System.out.println(LogAggregatorToolConstants.PROCESSING_MESSAGE);
-            int invalidcount = 0;
-            int validcount = 0;
+            int Invalidcount = 0;
+            int Validcount = 0;
+            int Count=0;
             for (String fileName : files) {
+                Count++;
                 if (!fileName.endsWith(".log")) {
-                    invalidcount++;
+                    Invalidcount++;
                     continue;
                 } else {
-                    validcount++;
+                    Validcount++;
                 }
             }
-            System.out.println(LogAggregatorToolConstants.INVALID_FILES + invalidcount);
-            System.out.println(LogAggregatorToolConstants.VALID_FILES + validcount);
+            System.out.println(LogAggregatorToolConstants.TOTAL_FILES+Count);
+            System.out.println(LogAggregatorToolConstants.INVALID_FILES + Invalidcount);
+            System.out.println(LogAggregatorToolConstants.VALID_FILES + Validcount);
         }
     }
 }
