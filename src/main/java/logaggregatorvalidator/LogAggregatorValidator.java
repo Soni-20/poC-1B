9dad68fbb791dd971a19102c385b0465c48b74c8
soni <sonisebastian99@gmail.com>
2025-03-13 09:09:07 +0530

    Added LogAggregatorValidator.java inside logaggregatortoolvalidator package

--- src/main/java/logaggregatorvalidator/LogAggregatorValidator.java
+++ src/main/java/logaggregatorvalidator/LogAggregatorValidator.java
@@ -1,35 +1,30 @@
-package logaggregatortool;
+package logaggregatorvalidator;
 
 import logaggregatortoolconstants.LogAggregatorToolConstants;
 
 import java.io.File;
 
+/**
+ * This class is responsible for validating the folder provided by the user.
+ * It checks whether the folder exists,whether it is empty .
+ */
 public class LogAggregatorValidator {
-    public void validatorFolder(File userInputPath) {
-        String[] files = userInputPath.list();
-        if (!(userInputPath.exists())) {
+    // Validate user-provided folder path
+    public boolean isValidFolder(File userInputPath) {
+        // Check if the folder exists
+        if (!userInputPath.exists() || !userInputPath.isDirectory()) {
             System.out.println(LogAggregatorToolConstants.INVALID_FOLDER);
-            return;
+            return false;
         }
-        if (userInputPath.length() == 0) {
+        return true;
+    }
+    // Check if the folder is empty
+    public boolean isFolderEmpty(File userInputPath) {
+        String[] files = userInputPath.list();
+        if (files.length == 0) {
             System.out.println(LogAggregatorToolConstants.FOLDER_EMPTY);
-        } else {
-            System.out.println(LogAggregatorToolConstants.PROCESSING_MESSAGE);
-            int invalidCount = 0;
-            int validCount = 0;
-            int Count=0;
-            for (String fileName : files) {
-                Count++;
-                if (!fileName.endsWith(".log")) {
-                    invalidCount++;
-                    continue;
-                } else {
-                    validCount++;
-                }
-            }
-            System.out.println(LogAggregatorToolConstants.TOTAL_FILES+Count);
-            System.out.println(LogAggregatorToolConstants.INVALID_FILES + invalidCount);
-            System.out.println(LogAggregatorToolConstants.VALID_FILES + validCount);
+            return true;
         }
+        return false;
     }
 }
