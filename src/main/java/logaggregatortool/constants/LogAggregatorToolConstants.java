edc85caae856ba5952218b266b729a01b5f3fff4
soni <sonisebastian99@gmail.com>
2025-03-14 12:13:55 +0530

    Modified code

--- src/main/java/logaggregatortool/constants/LogAggregatorToolConstants.java
+++ src/main/java/logaggregatortool/constants/LogAggregatorToolConstants.java
@@ -2,12 +2,13 @@ package logaggregatortool.constants;
 
 /**
  * This class defines constants used in the Log Aggregator Tool.
- * It contains various string messages used for logging and validation.*/
+ * It contains various string messages used for logging and validation.
+ */
 public class LogAggregatorToolConstants {
-    public static final String ERROR_OCCURED = "Error:No Folder path provided.please specify.Please provide a valid folder path.";
+    public static final String ERROR_OCCURED = "No Folder path provided.";
     public static final String FOLDER_EMPTY = "Folder is Empty.";
     public static final String INVALID_FILES = "Count of invalid files:";
-    public static final String INVALID_FOLDER = "Invalid_Folderpath .please provide a valid folderpath.";
+    public static final String INVALID_FOLDER = "Invalid Folderpath .please provide a valid folderpath.";
     public static final String LOG_EXTENSION = ".log";
     public static final String PROCESSING_MESSAGE = "Processing";
     public static final String TOTAL_FILES = "Total count of files:";
