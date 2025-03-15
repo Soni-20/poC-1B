b0a00e8dd93fbc82fefef4007613af0864300d2a
soni <sonisebastian99@gmail.com>
2025-03-15 11:35:14 +0530

    code modified

--- src/main/java/logaggregatortool/constants/LogAggregatorToolConstants.java
+++ src/main/java/logaggregatortool/constants/LogAggregatorToolConstants.java
@@ -6,11 +6,20 @@ package logaggregatortool.constants;
  */
 public class LogAggregatorToolConstants {
     public static final String ERROR_OCCURED = "No Folder path provided.";
+    public static final String FILE_PROCESSING_FAILED = "File Processing failed";
+    public static final String FILE_PROCESSING_SUCCESS = "File Processing success";
+    public static final String FILE_NAME_DATETIME_FORMAT = "yyyy-MM-dd_HH_mm_ss";
     public static final String FOLDER_EMPTY = "Folder is Empty.";
     public static final String INVALID_FILES = "Count of invalid files:";
     public static final String INVALID_FOLDER = "Invalid Folderpath .please provide a valid folderpath.";
     public static final String LOG_EXTENSION = ".log";
+    public static final String OUTPUT_DIRECTORY = "C:\\Internship\\Sample";
     public static final String PROCESSING_MESSAGE = "Processing";
+    public static final String REGEX_DATETIME_PATTERN = "\\d{4}-\\d{2}-\\d{2}";
+    public static final String SIMPLE_DATE_TIME_PATTERN = "MM/dd/yyyy HH:mm:ss:sss";
+    public static final String SORTED_FILE_PATH = "Sorted File Path : ";
+    public static final String SORTED_FILE_NAME = "\\LogAggegator_Merged_File ";
     public static final String TOTAL_FILES = "Total count of files:";
     public static final String VALID_FILES = "Count of valid Files:";
+    public static final String NEW_LINE = "\n";
 }
