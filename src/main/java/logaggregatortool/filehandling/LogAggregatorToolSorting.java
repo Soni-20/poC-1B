b45e14afac7d14128dfc4071ef20e44b18c5596a
soni <sonisebastian99@gmail.com>
2025-03-15 13:47:42 +0530

    Sort all logs based on date and time

--- src/main/java/logaggregatortool/filehandling/LogAggregatorToolSorting.java
+++ src/main/java/logaggregatortool/filehandling/LogAggregatorToolSorting.java
@@ -14,27 +14,25 @@ public class LogAggregatorToolSorting {
 
     /**
      * Method to sort all logs based on date and time.
-     *
-     * @param userFilePath Path to the log file.
      * @return Sorted ArrayList of log data.
      */
-    public ArrayList<String> logAggregatorToolSorting(String userFilePath) {
-        LogAggregatorToolReading logReader = new LogAggregatorToolReading();
-        ArrayList<String> fileData = logReader.readLogData(userFilePath);
-        SimpleDateFormat dateFormat = new SimpleDateFormat(LogAggregatorToolConstants.SIMPLE_DATE_TIME_PATTERN);
-        Collections.sort(fileData, (logLineDate1, logLineDate2) -> {
-            try {
-                if (logLineDate1.matches(LogAggregatorToolConstants.REGEX_DATETIME_PATTERN) &&
-                        logLineDate2.matches(LogAggregatorToolConstants.REGEX_DATETIME_PATTERN)) {
-                    Date date1 = dateFormat.parse(logLineDate1);
-                    Date date2 = dateFormat.parse(logLineDate2);
-                    return date1.compareTo(date2);
+
+        public ArrayList<String> sortLogData(ArrayList<String> fileData) {
+            SimpleDateFormat dateFormat = new SimpleDateFormat(LogAggregatorToolConstants.SIMPLE_DATE_TIME_PATTERN);
+            Collections.sort(fileData, (logLineDate1, logLineDate2) -> {
+                try {
+                    if (logLineDate1.matches(LogAggregatorToolConstants.REGEX_DATETIME_PATTERN) &&
+                            logLineDate2.matches(LogAggregatorToolConstants.REGEX_DATETIME_PATTERN)) {
+                        Date date1 = dateFormat.parse(logLineDate1);
+                        Date date2 = dateFormat.parse(logLineDate2);
+                        return date1.compareTo(date2);
+                    }
+                } catch (Exception e) {
+                    // If parsing fails, leave the order unchanged.
+                    return 0;
                 }
-            } catch (Exception e) {
                 return 0;
-            }
-            return 0;
-        });
-        return fileData;
-    }
+            });
+            return fileData;
+        }
 }
