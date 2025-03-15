f50a3bc546bd0dc0cf4389e6fcf955e6902456e8
soni <sonisebastian99@gmail.com>
2025-03-15 13:44:12 +0530

    call all the log processing class

--- src/main/java/logaggregatortool/filehandling/LogAggregatorToolProcessFiles.java
+++ src/main/java/logaggregatortool/filehandling/LogAggregatorToolProcessFiles.java
@@ -2,29 +2,42 @@ package logaggregatortool.filehandling;
 
 import logaggregatortool.constants.LogAggregatorToolConstants;
 
+import java.util.ArrayList;
+
+import static logaggregatortool.constants.LogAggregatorToolConstants.NEW_LINE;
+
 /**
  * logprocessor class for calling logreader,logsorter,logwriter
  */
 public class LogAggregatorToolProcessFiles {
     /**
      * call all the logprocessing classes and verify if file processing is succes or not
-     *
-     * @param args
      */
-    public void logAggregatorProcessFiles(String[] args) {
-        try {
-            String userFilePath = args[0];
-            LogAggregatorToolSorting logFileSorter = new LogAggregatorToolSorting();
-            LogAggregatorToolWriting logFileWriter = new LogAggregatorToolWriting();
-            logFileSorter.logAggregatorToolSorting(userFilePath);
-            boolean isFileProcessed = logFileWriter.writeLogFile(userFilePath);
-            if (isFileProcessed) {
-                System.out.println(LogAggregatorToolConstants.FILE_PROCESSING_SUCCESS + "\n" + LogAggregatorToolConstants.SORTED_FILE_PATH + logFileWriter.sortedLogPath);
-            } else {
-                System.out.println(LogAggregatorToolConstants.FILE_PROCESSING_FAILED);
+    public void logAggregatorProcessFiles(String[] args){
+            try {
+                String userFilePath = args[0];
+
+                // Read log data from the files in the directory.
+                LogAggregatorToolReading logReader = new LogAggregatorToolReading();
+                ArrayList<String> fileData = logReader.logAggregatorToolReading(userFilePath);
+
+                // Sort the read log data.
+                LogAggregatorToolSorting logSorter = new LogAggregatorToolSorting();
+                ArrayList<String> sortedData = logSorter.sortLogData(fileData);
+
+                // Write the sorted log data to a file.
+                LogAggregatorToolWriting logWriter = new LogAggregatorToolWriting();
+                boolean isFileProcessed = logWriter.writeLogFile(sortedData);
+
+                if (isFileProcessed) {
+                    System.out.println(LogAggregatorToolConstants.FILE_PROCESSING_SUCCESS + NEW_LINE +
+                            LogAggregatorToolConstants.SORTED_FILE_PATH + logWriter.sortedLogPath);
+                } else {
+                    System.out.println(LogAggregatorToolConstants.FILE_PROCESSING_FAILED);
+                }
+            } catch (Exception exception) {
+                exception.printStackTrace();
             }
-        } catch (Exception exception) {
-            exception.printStackTrace();
         }
     }
-}
+
