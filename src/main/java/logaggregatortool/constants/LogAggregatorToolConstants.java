355ff6e52bb5d2bceb376437ce270c6e14309601
soni <sonisebastian99@gmail.com>
2025-03-15 15:03:34 +0530

    constants added

--- src/main/java/logaggregatortool/constants/LogAggregatorToolConstants.java
+++ src/main/java/logaggregatortool/constants/LogAggregatorToolConstants.java
@@ -5,19 +5,20 @@ package logaggregatortool.constants;
  * It contains various string messages used for logging and validation.
  */
 public class LogAggregatorToolConstants {
-    public static final String ERROR_OCCURED = "No Folder path provided.";
-    public static final String READ_ERROR="Error reading file:";
-    public static final String DIRECTORY_EMPTY="No files found in the specified directory:";
+
     public static final String DIRECTORY_CREATION_FAILED="Failed to create output directory:";
+    public static final String DIRECTORY_EMPTY="No files found in the specified directory:";
+    public static final String ERROR_OCCURED = "No Folder path provided.";
+    public static final String FILE_NAME_DATETIME_FORMAT = "yyyy-MM-dd_HH_mm_ss";
     public static final String FILE_PROCESSING_FAILED = "File Processing failed";
     public static final String FILE_PROCESSING_SUCCESS = "File Processing success";
-    public static final String FILE_NAME_DATETIME_FORMAT = "yyyy-MM-dd_HH_mm_ss";
     public static final String FOLDER_EMPTY = "Folder is Empty.";
     public static final String INVALID_FILES = "Count of invalid files:";
     public static final String INVALID_FOLDER = "Invalid Folderpath .please provide a valid folderpath.";
     public static final String LOG_EXTENSION = ".log";
     public static final String OUTPUT_DIRECTORY = "C:\\Internship\\Sample";
     public static final String PROCESSING_MESSAGE = "Processing";
+    public static final String READ_ERROR="Error reading file:";
     public static final String REGEX_DATETIME_PATTERN = "\\d{4}-\\d{2}-\\d{2}";
     public static final String SIMPLE_DATE_TIME_PATTERN = "MM/dd/yyyy HH:mm:ss:sss";
     public static final String SORTED_FILE_PATH = "Sorted File Path : ";
