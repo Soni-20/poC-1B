f12ac402a938590996fa8a8d2aa61b352348b800
soni <sonisebastian99@gmail.com>
2025-03-17 00:56:00 +0530

    code Modified

--- src/main/java/logaggregatortool/constants/LogAggregatorToolConstants.java
+++ src/main/java/logaggregatortool/constants/LogAggregatorToolConstants.java
@@ -16,14 +16,14 @@ public class LogAggregatorToolConstants {
     public static final String INVALID_FILES = "Count of invalid files:";
     public static final String INVALID_FOLDER = "Invalid Folderpath .please provide a valid folderpath.";
     public static final String LOG_EXTENSION = ".log";
+    public static final String NEW_LINE = "\n";
     public static final String OUTPUT_DIRECTORY = "C:\\Internship\\Sample";
     public static final String PROCESSING_MESSAGE = "Processing";
     public static final String READ_ERROR="Error reading file:";
     public static final String REGEX_DATETIME_PATTERN = "\\d{4}-\\d{2}-\\d{2}";
     public static final String SIMPLE_DATE_TIME_PATTERN = "MM/dd/yyyy HH:mm:ss:sss";
     public static final String SORTED_FILE_PATH = "Sorted File Path : ";
-    public static final String SORTED_FILE_NAME = "\\LogAggegator_Merged_File ";
+    public static final String SORTED_LOG_FILE_NAME = "\\LogAggegator_Merged_File ";
     public static final String TOTAL_FILES = "Total count of files:";
     public static final String VALID_FILES = "Count of valid Files:";
-    public static final String NEW_LINE = "\n";
 }
