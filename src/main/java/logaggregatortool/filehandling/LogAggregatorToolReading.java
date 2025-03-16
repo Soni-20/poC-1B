b27028fd8a20776650e9533a28c935c9d36e81e4
soni <sonisebastian99@gmail.com>
2025-03-17 00:58:02 +0530

    code modified

--- src/main/java/logaggregatortool/filehandling/LogAggregatorToolReading.java
+++ src/main/java/logaggregatortool/filehandling/LogAggregatorToolReading.java
@@ -15,27 +15,27 @@ public class LogAggregatorToolReading {
      * @param logFilePath Path to the folder containing log files.
      * @return ArrayList containing all merged log data.
      */
-        public ArrayList<String>logAggregatorToolReading(String userLogFilePath) {
-            ArrayList<String> fileData = new ArrayList<>();
-            File userFolder = new File(userLogFilePath);
-            String[] folderContents = userFolder.list();
+    public ArrayList<String> logAggregatorToolReading(String userLogFilePath) {
+        ArrayList<String> fileData = new ArrayList<>();
+        File userFolder = new File(userLogFilePath);
+        String[] folderContents = userFolder.list();
 
-            if (folderContents == null) {
-                throw new RuntimeException(DIRECTORY_CREATION_FAILED + userLogFilePath);
-            }
-            for (String fileName : folderContents) {
-                File logFile = new File(userLogFilePath, fileName);
-                if (logFile.isFile()) {
-                    try (BufferedReader reader = new BufferedReader(new FileReader(logFile))) {
-                        String line;
-                        while ((line = reader.readLine()) != null) {
-                            fileData.add(line);
-                        }
-                    } catch (IOException exception) {
-                        throw new RuntimeException(READ_ERROR + fileName, exception);
+        if (folderContents == null) {
+            throw new RuntimeException(DIRECTORY_CREATION_FAILED + userLogFilePath);
+        }
+        for (String fileName : folderContents) {
+            File logFile = new File(userLogFilePath, fileName);
+            if (logFile.isFile()) {
+                try (BufferedReader reader = new BufferedReader(new FileReader(logFile))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        fileData.add(line);
                     }
+                } catch (IOException exception) {
+                    throw new RuntimeException(READ_ERROR + fileName, exception);
                 }
             }
-            return fileData;
         }
+        return fileData;
+    }
 }
