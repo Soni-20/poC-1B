98c26b69678d8b3baf2b5717f8b6ed6381566fa5
soni <sonisebastian99@gmail.com>
2025-03-18 06:53:30 +0530

    code modified

--- src/main/java/logaggregatortool/filehandling/LogAggregatorToolWriting.java
+++ src/main/java/logaggregatortool/filehandling/LogAggregatorToolWriting.java
@@ -4,20 +4,20 @@ package logaggregatortool.filehandling;
 import logaggregatortool.constants.LogAggregatorToolConstants;
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import static logaggregatortool.constants.LogAggregatorToolConstants.DIRECTORY_EMPTY;
+import java.util.Scanner;
 
 /**
  * Class for writing the sorted log data to a single log file.
  */
 public class LogAggregatorToolWriting {
 
-    private final String outputDirectory = LogAggregatorToolConstants.OUTPUT_DIRECTORY;
     private final String currentDateTime = new SimpleDateFormat(LogAggregatorToolConstants.FILE_NAME_DATETIME_FORMAT).format(new Date());
-    public String sortedLogPath = outputDirectory + LogAggregatorToolConstants.SORTED_LOG_FILE_NAME + currentDateTime + LogAggregatorToolConstants.LOG_EXTENSION;
+    public String outputFilePath;
+    public String sortedLogPath = outputFilePath + LogAggregatorToolConstants.SORTED_LOG_FILE_NAME + currentDateTime + LogAggregatorToolConstants.LOG_EXTENSION;
+    String outputFolder;
 
     /**
      * Writes the provided sorted log data to a log file.
@@ -25,22 +25,41 @@ public class LogAggregatorToolWriting {
      * @return true if the file is successfully written, otherwise false.
      */
     public boolean writeLogFile(ArrayList<String> sortedData) {
-        File outputDir = new File(outputDirectory);
-        if (!outputDir.exists() && !outputDir.mkdirs()) {
-            System.err.println(DIRECTORY_EMPTY + outputDirectory);
-            return false;
-        }
         try {
-            File sortedFile = new File(sortedLogPath);
-            try (FileWriter writer = new FileWriter(sortedFile)) {
-                for (String line : sortedData) {
-                    writer.write(line + LogAggregatorToolConstants.NEW_LINE);
+            String sortedFilePath = sortedLogPath;
+            System.out.println(LogAggregatorToolConstants.USER_OUTPUT_FOLDER_PATH);
+            if (!verifyUserInputpath()) {
+                verifyUserInputpath();
+            }
+            {
+                File sortedFile = new File(sortedLogPath);
+                File file = new File(outputFolder + sortedFilePath);
+                FileWriter writer = new FileWriter(file);
+                for (Object line : sortedData) {
+                    writer.write((String) line);
+                    writer.write(LogAggregatorToolConstants.NEW_LINE);
+                    outputFilePath = outputFolder + sortedFilePath;
                 }
             }
             return true;
-        } catch (IOException exception) {
+        } catch (Exception exception) {
             exception.printStackTrace();
+            return true;
+        }
+    }
+    private boolean verifyUserInputpath() {
+        Scanner scanner = new Scanner(System.in);
+        outputFolder = scanner.nextLine();
+        try {
+            File file = new File(outputFolder);
+            if (file.exists() || file.isDirectory()) {
+                return true;
+            }
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
             return false;
         }
+        System.out.println(LogAggregatorToolConstants.USER_OUTPUT_INVALID_PATH);
+        return false;
     }
 }
