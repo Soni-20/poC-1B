5ad387a27e5e6f1ce412bc57002fa540c78b913a
soni <sonisebastian99@gmail.com>
2025-03-18 06:52:54 +0530

    code modified

--- src/main/java/logaggregatortool/filehandling/LogAggregatorToolSorting.java
+++ src/main/java/logaggregatortool/filehandling/LogAggregatorToolSorting.java
@@ -1,12 +1,11 @@
 package logaggregatortool.filehandling;
 
 import logaggregatortool.constants.LogAggregatorToolConstants;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
-
 /**
  * Class for sorting the merged log data.
  */
@@ -15,20 +14,22 @@ public class LogAggregatorToolSorting {
      * Method to sort all logs based on date and time.
      * @return Sorted ArrayList of log data.
      */
-        public ArrayList<String> sortLogData(ArrayList<String> fileData) {
+    public ArrayList<String> sortLogData(ArrayList<String> fileData) {
             SimpleDateFormat dateFormat = new SimpleDateFormat(LogAggregatorToolConstants.SIMPLE_DATE_TIME_PATTERN);
-            Collections.sort(fileData, (logLineDate1, logLineDate2) -> {
-                try {
-                    if (logLineDate1.matches(LogAggregatorToolConstants.REGEX_DATETIME_PATTERN) &&
-                            logLineDate2.matches(LogAggregatorToolConstants.REGEX_DATETIME_PATTERN)) {
-                        Date date1 = dateFormat.parse(logLineDate1);
-                        Date date2 = dateFormat.parse(logLineDate2);
-                        return date1.compareTo(date2);
+            Collections.sort(fileData, new Comparator<String>() {
+                @Override
+                public int compare(String logLineDate1, String logLineDate2) {
+                    if (logLineDate1.matches(LogAggregatorToolConstants.REGEX_DATETIME_PATTERN) && logLineDate2.matches(LogAggregatorToolConstants.REGEX_DATETIME_PATTERN)) {
+                        try {
+                            Date date1 = dateFormat.parse(logLineDate1);
+                            Date date2 = dateFormat.parse(logLineDate2);
+                            return (date1.compareTo(date2));
+                        } catch (Exception e) {
+                            return 0;
+                        }
                     }
-                } catch (ParseException e) {
-                    System.out.println("Parsing failed:"+e.getMessage());
+                    return 0;
                 }
-                return 0;
             });
             return fileData;
         }
