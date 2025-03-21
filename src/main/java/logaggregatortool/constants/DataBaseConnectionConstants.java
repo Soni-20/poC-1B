912464e40e9c0aa57c8dde2db27ae79913ca393b
soni <sonisebastian99@gmail.com>
2025-03-21 16:23:06 +0530

    added application.properties

---
+++ src/main/java/logaggregatortool/constants/DataBaseConnectionConstants.java
@@ -0,0 +1,10 @@
+package logaggregatortool.constants;
+
+/**
+ * added constants DB_URL,DB_USERNAME,DB_PASSWORD
+ */
+public class DataBaseConnectionConstants {
+   public static final String DB_URL = "DB_URL";
+    public static final String DB_USERNAME = "DB_USERNAME";
+    public static final String DB_PASSWORD = "DB_PASSWORD";
+}
