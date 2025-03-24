c35ebb103d74d23600516282bdadcb07fecf9126
soni <sonisebastian99@gmail.com>
2025-03-24 12:07:19 +0530

    code modified

--- src/main/java/logaggregatortool/filehandling/LogAggregatorToolSorting.java
+++ src/main/java/logaggregatortool/filehandling/LogAggregatorToolSorting.java
@@ -2,36 +2,58 @@ package logaggregatortool.filehandling;
 
 import logaggregatortool.constants.LogAggregatorToolConstants;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Class for sorting the merged log data.
  */
 public class LogAggregatorToolSorting {
+    private static final Pattern yearMonthDayPattern = Pattern.compile(LogAggregatorToolConstants.REGEX_DATETIME_PATTERN_YMD);
+    private static final Pattern monthDayYearPattern = Pattern.compile(LogAggregatorToolConstants.REGEX_DATETIME_PATTERN_MDY);
+    /**
+     * method to extract the datetime from loglines using regular expression and simpledatetime format
+     *
+     * @param logLine
+     * @return
+     * @throws ParseException
+     */
+    private Date extractDate(String logLine) throws ParseException {
+        SimpleDateFormat yearMonthDayFormat = new SimpleDateFormat(LogAggregatorToolConstants.SIMPLE_DATE_TIME_PATTERN_YMD);
+        SimpleDateFormat monthDayYearFormat = new SimpleDateFormat(LogAggregatorToolConstants.SIMPLE_DATE_TIME_PATTERN_MDY);
+        Matcher yearMonthDayFormatMatcher = yearMonthDayPattern.matcher(logLine);
+        Matcher monthDayYearFormatMatcher = monthDayYearPattern.matcher(logLine);
+        if (yearMonthDayFormatMatcher.find()) {
+            return yearMonthDayFormat.parse(yearMonthDayFormatMatcher.group(1));
+        } else if (monthDayYearFormatMatcher.find()) {
+            return monthDayYearFormat.parse(monthDayYearFormatMatcher.group(1));
+        } else {
+            throw new ParseException("No valid date in logline" + logLine, 0);
+        }
+    }
     /**
      * Method to sort all logs based on date and time.
      *
      * @return Sorted ArrayList of log data.
      */
     public ArrayList<String> sortLogData(ArrayList<String> fileData) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat(LogAggregatorToolConstants.SIMPLE_DATE_TIME_PATTERN);
         Collections.sort(fileData, new Comparator<String>() {
             @Override
             public int compare(String logLineDate1, String logLineDate2) {
-                if (logLineDate1.matches(LogAggregatorToolConstants.REGEX_DATETIME_PATTERN) && logLineDate2.matches(LogAggregatorToolConstants.REGEX_DATETIME_PATTERN)) {
-                    try {
-                        Date date1 = dateFormat.parse(logLineDate1);
-                        Date date2 = dateFormat.parse(logLineDate2);
-                        return (date1.compareTo(date2));
-                    } catch (Exception exception) {
-                        exception.getMessage();
-                        return 0;
-                    }
+                try {
+                    Date date1 = extractDate(logLineDate1);
+                    Date date2 = extractDate(logLineDate2);
+                    return (date1.compareTo(date2));
+                } catch (ParseException exception) {
+                    System.out.println(exception.getMessage());
+                    return 0;
                 }
-                return 0;
             }
         });
         return fileData;
