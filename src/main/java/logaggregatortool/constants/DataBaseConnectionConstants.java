64c959f5e3fcb578cf4be131707d03ef5097d974
soni <sonisebastian99@gmail.com>
2025-03-24 11:15:09 +0530

    rearranged the constants

--- src/main/java/logaggregatortool/constants/DataBaseConnectionConstants.java
+++ src/main/java/logaggregatortool/constants/DataBaseConnectionConstants.java
@@ -5,6 +5,6 @@ package logaggregatortool.constants;
  */
 public class DataBaseConnectionConstants {
     public static final String DB_PASSWORD = "DB_PASSWORD";
-   public static final String DB_URL = "DB_URL";
+    public static final String DB_URL = "DB_URL";
     public static final String DB_USERNAME = "DB_USERNAME";
 }
